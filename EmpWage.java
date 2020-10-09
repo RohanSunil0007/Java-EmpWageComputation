50d2d41f2f9d76428c7058b4f053951f21f0df69
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-09 22:23:03 +0530

    Added : Daily Employee Wage Functionality

--- EmpWage.java
+++ EmpWage.java
@@ -4,14 +4,19 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
         final int isPresent=1;
+        final int ratePerHour= 20 ; 
         double empCheck= Math.floor(Math.random() * 10)%2;
-        if ( empCheck == isPresent )
+        int salary;
+        int EmpHr;
+        if ( empCheck == isPresent)
         {
-        	System.out.println("Employee is Present ");
+        	EmpHr=8;
+        	salary= EmpHr * ratePerHour ;
+            System.out.println("Employee is Present , so Salary is : " + salary);
         }
         else
-        {
-        	System.out.println("Employee is absent ");
+        { 
+        	System.out.println("Employee is absent , So no salary ");
         }
 	}
 
