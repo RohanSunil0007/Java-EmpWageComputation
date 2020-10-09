99ab60194b45d2a18ebc6b2128adb73b0da86abf
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-09 22:42:58 +0530

    Added : Part Time Functionality

--- EmpWage.java
+++ EmpWage.java
@@ -1,23 +1,31 @@
 //Welcome to Employee Wage Computation
 public class EmpWage {
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-        final int isPresent=1;
+        final int FullTime=1;
+        final int PartTime=2;
         final int ratePerHour= 20 ; 
-        double empCheck= Math.floor(Math.random() * 10)%2;
+        double empCheck= Math.floor(Math.random() * 10)%3;
         int salary;
         int EmpHr;
-        if ( empCheck == isPresent)
-        {
-        	EmpHr=8;
-        	salary= EmpHr * ratePerHour ;
-            System.out.println("Employee is Present , so Salary is : " + salary);
-        }
-        else
-        { 
-        	System.out.println("Employee is absent , So no salary ");
-        }
+        	if ( empCheck == FullTime )
+        	{
+        		EmpHr=8;
+        		salary= EmpHr * ratePerHour ;
+            System.out.println("Full Time Salary is : " + salary);
+        	}
+         else if (empCheck == PartTime )
+         {
+        		EmpHr=4;
+        		salary= EmpHr * ratePerHour ;
+            System.out.println("Part Time Salary is : " + salary);
+         }
+         else
+         {
+        		EmpHr=0;
+        		salary= EmpHr * ratePerHour ;
+        		System.out.println("Employee is absent , So salary : " + salary);
+         }
 	}
 
 }
