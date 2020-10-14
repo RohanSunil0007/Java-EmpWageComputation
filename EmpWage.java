7574cc16f4c0aad88d5ff53418fa4b189cd4422d
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-14 08:18:32 +0530

    Added : Employee wage computation for multiple companies

--- EmpWage.java
+++ EmpWage.java
@@ -1,28 +1,23 @@
 //Welcome to Employee Wage Computation
 public class EmpWage {
+   //CONSTANTS
+   public static final int FullTime=1;
+   public static final int PartTime=2;
 
-//CONSTANTS
-public static final int FullTime=1;
-public static final int PartTime=2;
-public static final int ratePerHours= 20 ;
-public static final int MaxworkingDays=20;
-public static final int MaxHrsPerMonth=100;
-
-
-     public static int EmployeeWageComputation() {
-      //VARIABLES
-       int salary=0;
-       int EmpHours=0;
-       int totalEmpHours=0;
-       int totalEmpDays=0;
+    void EmployeeWageComputation(String company , int ratePerHours , int MaxworkingDays , int MaxHrsPerMonth) {
+        System.out.println(company + " Wage Computation ");
+    	//VARIABLES
+        int salary=0;
+        int EmpHours=0;
+        int totalEmpHours=0;
+        int totalEmpDays=0;
 
         while (totalEmpHours < MaxHrsPerMonth && totalEmpDays < MaxworkingDays  )
         {
         	totalEmpDays++;
         	int empCheck= (int) Math.floor(Math.random() * 10)%3;
-
         	//Attendance Check
-        	switch( empCheck )
+        	switch( empCheck ) 
         	{
         	case FullTime :
         		EmpHours=8;
@@ -38,12 +33,19 @@ public static final int MaxHrsPerMonth=100;
         	salary= EmpHours * ratePerHours ;
         	totalEmpHours += EmpHours;
         	System.out.println("Day " +totalEmpDays + " Employee work hours " +EmpHours +" and daily wage is " +salary ); 
-      	}
-     		System.out.println("Total Salary for a month is : " +totalEmpHours * ratePerHours );
-     		return totalEmpHours * ratePerHours;
-     }
-     public static void main(String[] args) {
-    	System.out.println("Welcome to Employee Wage Computation");
-    	EmployeeWageComputation();
       }
+     System.out.println("Total Salary for a month is : " +totalEmpHours * ratePerHours );
+
+    }
+    public static void main(String[] args) {
+    	System.out.println("Welcome to Employee Wage Computation");
+
+    	EmpWage company1=new EmpWage();
+    	company1.EmployeeWageComputation("Amazon", 20, 20, 100);
+
+    	System.out.println(" ");
+
+   	EmpWage company2=new EmpWage();
+    	company2.EmployeeWageComputation("Walmart", 20,30, 100);
+    }
 }
